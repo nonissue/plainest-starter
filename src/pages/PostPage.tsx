72d049bae16cee6e4200e0dc7d24ce2154f25515
Andy Williams <andy@nonissue.org>
2020-01-15 01:39:24 -0700

    useAxios req responses get stored as named objects

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link, useParams } from 'react-router-dom';
 import { useAxios } from '../lib/useAxios';
+import { Loading, Error } from '../components';
 
 type PostState = {
   title: string;
@@ -38,35 +39,103 @@ That way,  we don't need two different components both fetching
 Would be nice to use reduecers for each slice of state
 */
 
+interface Response {
+  loading: boolean;
+  error: { code?: number; msg?: string };
+}
+
+interface Post extends Response {
+  data: {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  };
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface Author extends Response {
+  data: {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: Company;
+  };
+}
+
+interface Error {
+  code?: number;
+  msg?: string;
+}
+
+/*
+Tests:
+- errors are handled correctly when returned from axios
+- comment mounts
+- figure out how to check for data racing conditions?
+*/
+
 export const PostPage: React.FC = () => {
   // eslint-disable-next-line prefer-const
   let { id } = useParams();
 
-  const { data: postData } = useAxios(
+  const post = {} as Post;
+  const author = {} as Author;
+
+  ({ data: post.data, loading: post.loading, error: post.error } = useAxios(
     {
       url: `/.netlify/functions/posts-fetch-one/${id}`,
     },
     { test: 'test', test2: 'test3' },
-  );
+  ));
 
-  const { data: authorData } = useAxios({
-    url: postData ? `/.netlify/functions/users-fetch-one/${postData.userId}` : null,
-  });
+  ({ data: author.data, loading: author.loading, error: author.error } = useAxios({
+    url: post.data ? `/.netlify/functions/users-fetch-one/${post.data.userId}` : null,
+  }));
+
+  if (post.error || author.error) {
+    const error = post.error || author.error;
+    return <Error code={error.code} msg={error.msg} />;
+  }
+
+  if (post.loading || author.loading) {
+    return <Loading />;
+  }
 
   return (
     <StyledPost>
-      {postData ? (
-        <>
-          <h1>
-            <Link to={`/posts/${postData.id}`}>{postData.title}</Link>
-          </h1>
-          <h4>— by {authorData ? authorData.name : 'Loading'}</h4>
-          {/* <AuthorInfo userId={post.userId} /> */}
-          <p>{postData.body}</p>
-        </>
-      ) : (
-        <>Loading...</>
-      )}
+      <>
+        <h2>
+          <Link to={`/posts/${post.data.id}`}>{post.data.title}</Link>
+        </h2>
+        <h4>
+          — by <Link to={`/users/${author.data.id}`}>{author.data.name}</Link>
+        </h4>
+        <p>{`${post.data.body}`.repeat(2)}</p>
+        <p>{`${post.data.body}`.repeat(3)}</p>
+      </>
     </StyledPost>
   );
 };
@@ -78,11 +147,53 @@ const StyledPost = styled.div`
   max-width: 600px;
   margin: 0 auto;
 
-  h1 {
+  text-align: left;
+  box-shadow: 0 0.7px 1px rgba(0, 0, 0, 0.1), 0 1.8px 2.3px rgba(0, 0, 0, 0.029),
+    0 3.4px 4.4px rgba(0, 0, 0, 0.024), 0 6px 7.8px rgba(0, 0, 0, 0.02);
+  border: 0.5px solid #e2e8f0;
+  background: #fff;
+  padding: 1em;
+
+  h2 {
+    font-weight: 700;
+    font-size: 2em;
+
+    margin-top: 0;
+    margin-bottom: 0;
+    margin-bottom: 0.25rem;
+    text-transform: capitalize;
+    line-height: 1.22em;
+
+    letter-spacing: -0.03em;
+    a {
+      color: #2d3748;
+    }
+  }
+  h4 {
+    font-weight: 400;
+    text-transform: uppercase;
+    padding: 0;
+    margin: 0;
+    color: #cbd5e0;
+    /* color: #cbd5e0; */
+
+    a {
+      background: transparent;
+      font-family: ${props => props.theme.fonts.body};
+      /* font-weight: 500; */
+      box-shadow: none;
+      /* border: none; */
+      color: #718096;
+      color: #a0aec0;
+    }
+  }
+
+  /* h1 {
     font-weight: 700;
     text-transform: capitalize;
     line-height: 1.2em;
     margin-bottom: 0.2em;
+    margin-top: 0em;
     letter-spacing: -0.03em;
     text-align: left;
   }
@@ -94,10 +205,11 @@ const StyledPost = styled.div`
     color: #555;
     margin-left: 0.5em;
     text-align: left;
-  }
+  } */
   p {
     font-size: 0.95em;
     line-height: 1.6em;
+    margin-bottom: 0;
 
     :first-letter {
       text-transform: capitalize;
